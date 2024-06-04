61d67df2c9baceb630b702075ed4352ed44ad0a3
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-06-03 23:30:59 -0500

    addition of previous searches as buttons that get appended to a container using Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,40 @@
-const citySearchEl = $('#citySearch')
-const SearchBtnEl = $('#searchBtn')
-let cityList = []
+const citySearchEl = $('#citySearch');
+const searchBtnEl = $('#searchBtn');
+const previousCitiesEl = $('#previousCitySearches'); // Container for displaying previous city searches
+let cityList = [];
 
 function loadPreviousCitySearches() {
-    JSON.parse(localStorage.getItem('cityList')) || [];
-    return cityList
+    cityList = JSON.parse(localStorage.getItem('cityList')) || [];
+    displayPreviousCitySearches();
 }
 
 function savePreviousCitySearches() {
     localStorage.setItem('cityList', JSON.stringify(cityList));
 }
 
-SearchBtnEl.on('click', function() {
-    loadPreviousCitySearches();
-    searchInput = citySearchEl.val().trim();
-    if(searchInput) {
-    cityList.push(searchInput);
-    savePreviousCitySearches();
-    }
-})
-
-function init() {
-loadPreviousCitySearches()
+function displayPreviousCitySearches() {
+    previousCitiesEl.empty();
+    cityList.forEach(city => {
+        const cityBtn = $('<button>')
+            .addClass('btn city-btn bg-dark text-light my-1 w-100')
+            .text(city);
+        previousCitiesEl.append(cityBtn);
+    });
 }
 
-init()
+searchBtnEl.on('click', function () {
+    loadPreviousCitySearches();
+    const searchInput = citySearchEl.val().trim();
+    if (searchInput) {
+        cityList.push(searchInput);
+        savePreviousCitySearches();
+        citySearchEl.val(''); // Clear the input field
+        displayPreviousCitySearches(); // Update the displayed list
+    }
+});
+
+function init() {
+    loadPreviousCitySearches();
+}
+
+init();
