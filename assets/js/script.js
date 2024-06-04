58295e81176471718ae7118f54df5d0c82054f32
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-06-04 18:23:57 -0500

    finalization of the geo lat/lon function for the initial search

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,8 @@ function displayPreviousCitySearches() {
     cityList.forEach(city => {
         const cityBtn = $('<button>')
             .addClass('btn city-btn bg-info text-light my-2 w-100')
-            .attr('id', city)
-            .text(city);
+            .attr('id', city.name)
+            .text(city.name);
         previousCitiesEl.append(cityBtn);
     });
 }
@@ -27,25 +27,23 @@ function geoLocationGet(searchInput) {
     const url = 'https://api.openweathermap.org/geo/1.0/direct?q=';
     const url2 = '&limit=1&appid=14462ac933eb3455a10c1a0bd20bdd1e';
     const urlInput = url + searchInput + url2;
-    const geoCoordinates = ''
 
-    fetch(urlInput)
+    return fetch(urlInput)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             const cityLat = data[0].lat
             const cityLon = data[0].lon
-            
-            
+
+
             localStorage.setItem('cityLat', JSON.stringify(cityLat))
             localStorage.setItem('cityLon', JSON.stringify(cityLon))
+
+            return data
         });
 
 
-}
-
-function forecastGet() {
 
 }
 
@@ -57,26 +55,25 @@ searchBtnEl.on('click', function () {
     loadPreviousCitySearches();
     const searchInput = citySearchEl.val().trim();
     if (searchInput) {
-        geoLocationGet(searchInput);
+        geoLocationGet(searchInput).then(function (data) {
 
-        const cityLat = JSON.parse(localStorage.getItem('cityLat'))
-        const cityLon = JSON.parse(localStorage.getItem('cityLon'))
+            const cityLat = JSON.parse(localStorage.getItem('cityLat'))
+            const cityLon = JSON.parse(localStorage.getItem('cityLon'))
 
-        cityDetails = {
-            'name': searchInput,
-            'lat': cityLat,
-            'lon': cityLon,
-        }
-        cityList.push(cityDetails);
-        cityDetails = ''
-        savePreviousCitySearches();
-        citySearchEl.val(''); // Clear the input field
-        displayPreviousCitySearches(); // Update the displayed list
-        console.log(cityDetails)
+            cityDetails = {
+                'name': searchInput,
+                'lat': cityLat,
+                'lon': cityLon,
+            }
+            cityList.push(cityDetails);
+            cityDetails = ''
+            savePreviousCitySearches();
+            citySearchEl.val(''); // Clear the input field
+            displayPreviousCitySearches(); // Update the displayed list
+            console.log(cityDetails)
+
+        })
     }
-
-    
-
 });
 
 previousCitiesEl.on('click', function () {
