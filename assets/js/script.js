e705260c77b24142715ffa6df6ed19ff6cb1ba47
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-06-07 23:15:21 -0500

    addition of the forecast api and the associated functionality with it and both a new search and previous city search.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ const searchBtnEl = $('#searchBtn');
 const previousCitiesEl = $('#previousCitySearches'); // Container for displaying previous city searches
 const apiKey = '14462ac933eb3455a10c1a0bd20bdd1e';
 const dailyWeatherEl = $('#dailyWeatherSection');
+const forecastDay1 = $('#forecast-1');
+const forecastDay2 = $('#forecast-2');
+const forecastDay3 = $('#forecast-3');
+const forecastDay4 = $('#forecast-4');
+const forecastDay5 = $('#forecast-5');
 let cityList = [];
 
 
@@ -22,7 +27,7 @@ function displayPreviousCitySearches() {
             .addClass('btn city-btn bg-info text-light my-2 w-100')
             .attr('id', city.name)
             .text(city.name)
-            .click(function() {
+            .click(function () {
                 localStorage.setItem('prevCity', JSON.stringify($(this).text()))
             })
         previousCitiesEl.append(cityBtn);
@@ -56,6 +61,7 @@ function currentWeatherGet(latitude, longitude, apiKey) {
             return response.json();
         })
         .then(function (data) {
+            dailyWeatherEl.empty()
             const weatherImageBase = 'https://openweathermap.org/img/wn/'
             const weatherImageModifier = data.weather[0].icon
             const weatherImageSuffix = '.png'
@@ -75,7 +81,7 @@ function currentWeatherGet(latitude, longitude, apiKey) {
             const dailyTemp = $('<h3>')
                 .text(`Temp: ${data.main.temp}°F`);
             const dailyWind = $('<h3>')
-                .text(`Wind: ${data.main.wind} MPH`);
+                .text(`Wind: ${data.wind.speed} MPH`);
             const dailyHumidity = $('<h3>')
                 .text(`Humidity: ${data.main.humidity} %`);
 
@@ -87,6 +93,155 @@ function currentWeatherGet(latitude, longitude, apiKey) {
             dailyWeatherEl.append(dailyHumidity);
 
 
+        })
+        .then(function (data) {
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data)
+                    forecastDay1.empty()
+                    forecastDay2.empty()
+                    forecastDay3.empty()
+                    forecastDay4.empty()
+                    forecastDay5.empty()
+
+                    const weatherImageBase = 'https://openweathermap.org/img/wn/'
+                    const weatherImageSuffix = '.png'
+
+                    const day1Date = $('<h3>')
+                        .text(`(${dayjs(data.list[0].dt * 1000).format("MM/DD/YYYY")})`)
+                        .addClass('text-light text-center')
+                    const day2Date = $('<h3>')
+                        .text(`(${dayjs(data.list[8].dt * 1000).format("MM/DD/YYYY")})`)
+                        .addClass('text-light text-center')
+                    const day3Date = $('<h3>')
+                        .text(`(${dayjs(data.list[16].dt * 1000).format("MM/DD/YYYY")})`)
+                        .addClass('text-light text-center')
+                    const day4Date = $('<h3>')
+                        .text(`(${dayjs(data.list[24].dt * 1000).format("MM/DD/YYYY")})`)
+                        .addClass('text-light text-center')
+                    const day5Date = $('<h3>')
+                        .text(`(${dayjs(data.list[32].dt * 1000).format("MM/DD/YYYY")})`)
+                        .addClass('text-light text-center')
+
+                    const day1Image = $('<img>')
+                        .attr({
+                            'src': `${weatherImageBase}${data.list[0].weather[0].icon}${weatherImageSuffix}`,
+                            'id': 'weatherIcon'
+                        })
+                    const day2Image = $('<img>')
+                        .attr({
+                            'src': `${weatherImageBase}${data.list[8].weather[0].icon}${weatherImageSuffix}`,
+                            'id': 'weatherIcon'
+                        })
+                    const day3Image = $('<img>')
+                        .attr({
+                            'src': `${weatherImageBase}${data.list[16].weather[0].icon}${weatherImageSuffix}`,
+                            'id': 'weatherIcon'
+                        })
+                    const day4Image = $('<img>')
+                        .attr({
+                            'src': `${weatherImageBase}${data.list[24].weather[0].icon}${weatherImageSuffix}`,
+                            'id': 'weatherIcon'
+                        })
+                    const day5Image = $('<img>')
+                        .attr({
+                            'src': `${weatherImageBase}${data.list[32].weather[0].icon}${weatherImageSuffix}`,
+                            'id': 'weatherIcon'
+                        })
+
+                    const day1Temp = $('<h4>')
+                        .text(`Temp: ${data.list[0].main.temp}°F`)
+                        .addClass('text-light text-center');
+                    const day2Temp = $('<h4>')
+                        .text(`Temp: ${data.list[8].main.temp}°F`)
+                        .addClass('text-light text-center');
+                    const day3Temp = $('<h4>')
+                        .text(`Temp: ${data.list[16].main.temp}°F`)
+                        .addClass('text-light text-center');
+                    const day4Temp = $('<h4>')
+                        .text(`Temp: ${data.list[24].main.temp}°F`)
+                        .addClass('text-light text-center');
+                    const day5Temp = $('<h4>')
+                        .text(`Temp: ${data.list[32].main.temp}°F`)
+                        .addClass('text-light text-center');
+
+                    const day1Wind = $('<h4>')
+                        .text(`Wind: ${data.list[0].wind.speed} MPH`)
+                        .addClass('text-light text-center');
+                    const day2Wind = $('<h4>')
+                        .text(`Wind: ${data.list[8].wind.speed} MPH`)
+                        .addClass('text-light text-center');
+                    const day3Wind = $('<h4>')
+                        .text(`Wind: ${data.list[16].wind.speed} MPH`)
+                        .addClass('text-light text-center');
+                    const day4Wind = $('<h4>')
+                        .text(`Wind: ${data.list[24].wind.speed} MPH`)
+                        .addClass('text-light text-center');
+                    const day5Wind = $('<h4>')
+                        .text(`Wind: ${data.list[32].wind.speed} MPH`)
+                        .addClass('text-light text-center');
+
+                    const day1Humidity = $('<h4>')
+                        .text(`Humidity: ${data.list[0].main.humidity} %`)
+                        .addClass('text-light text-center');
+                    const day2Humidity = $('<h4>')
+                        .text(`Humidity: ${data.list[8].main.humidity} %`)
+                        .addClass('text-light text-center');
+                    const day3Humidity = $('<h4>')
+                        .text(`Humidity: ${data.list[16].main.humidity} %`)
+                        .addClass('text-light text-center');
+                    const day4Humidity = $('<h4>')
+                        .text(`Humidity: ${data.list[24].main.humidity} %`)
+                        .addClass('text-light text-center');
+                    const day5Humidity = $('<h4>')
+                        .text(`Humidity: ${data.list[32].main.humidity} %`)
+                        .addClass('text-light text-center');
+
+                    const divider = $('<div>')
+                        .addClass('w-100')
+
+                    function dividerAdd() {
+                        forecastDay1.append(divider)
+                        forecastDay2.append(divider)
+                        forecastDay3.append(divider)
+                        forecastDay4.append(divider)
+                        forecastDay5.append(divider)
+                    }
+
+                    forecastDay1.append(day1Date)
+                    forecastDay2.append(day2Date)
+                    forecastDay3.append(day3Date)
+                    forecastDay4.append(day4Date)
+                    forecastDay5.append(day5Date)
+                    dividerAdd()
+                    forecastDay1.append(day1Image)
+                    forecastDay2.append(day2Image)
+                    forecastDay3.append(day3Image)
+                    forecastDay4.append(day4Image)
+                    forecastDay5.append(day5Image)
+                    dividerAdd()
+                    forecastDay1.append(day1Temp)
+                    forecastDay2.append(day2Temp)
+                    forecastDay3.append(day3Temp)
+                    forecastDay4.append(day4Temp)
+                    forecastDay5.append(day5Temp)
+                    dividerAdd()
+                    forecastDay1.append(day1Wind)
+                    forecastDay2.append(day2Wind)
+                    forecastDay3.append(day3Wind)
+                    forecastDay4.append(day4Wind)
+                    forecastDay5.append(day5Wind)
+                    dividerAdd()
+                    forecastDay1.append(day1Humidity)
+                    forecastDay2.append(day2Humidity)
+                    forecastDay3.append(day3Humidity)
+                    forecastDay4.append(day4Humidity)
+                    forecastDay5.append(day5Humidity)
+
+                })
         })
 }
 
@@ -144,7 +299,7 @@ previousCitiesEl.on('click', function () {
                 currentWeatherGet(lat, lon, apiKey)
             })
     }
-    
+
 })
 
 function init() {
